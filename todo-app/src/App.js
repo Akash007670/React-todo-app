71585518ad10764e7e225c73dd595600c643120d
Akash007670 <akaysharma894@gmail.com>
2021-01-15 18:10:24 +0530

    commit

--- todo-app/src/App.js
+++ todo-app/src/App.js
@@ -1,10 +1,19 @@
-import logo from './logo.svg';
+import React , {useState , useEffect} from 'react';
 import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
+
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
-      
+      <header>
+        <h1>Akash's Todo list</h1>
+      </header>
+      <Form  setInputText = {setInputText}/>
+      <TodoList />
     </div>
   );
 }
