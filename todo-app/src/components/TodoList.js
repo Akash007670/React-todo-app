5f663a498cd8ee91100e034caacb014426a8c3e1
Akash007670 <akaysharma894@gmail.com>
2021-01-16 22:38:52 +0530

    updated todo list

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,9 +1,15 @@
-import React from 'react'
+import React from 'react';
+import Todo  from './Todo';
 
-const TodoList = () => {
+const TodoList = ({todos}) => {
+    // console.log(todos);
     return (
         <div className = "todo-container">
-            <ul className = "todo-list"></ul>            
+            <ul className = "todo-list">
+                {todos.map(todo => (
+                    <Todo text = {todo.text}/>
+                ))}
+            </ul>            
         </div>
     )
 }
