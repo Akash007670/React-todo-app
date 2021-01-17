b7c78e4575837e5bdf8a45ab1e6cba411dab9e69
Akash007670 <akaysharma894@gmail.com>
2021-01-17 15:45:41 +0530

    Added statusHandler

--- todo-app/src/components/Form.js
+++ todo-app/src/components/Form.js
@@ -1,19 +1,23 @@
 import React from 'react'
 
-const Form = ({setInputText , todos ,setTodos, inputText}) => {
+const Form = ({setInputText , todos ,setTodos, inputText,setStatus}) => {
 
     const inputTextHandler = (e) => {
-        console.log(e.target.value);
         setInputText(e.target.value);
     }
 
     const submitTodoHandler = (e) => {
         e.preventDefault();
         setTodos([
-            ...todos , {text : inputText , completed : "false" ,id : Math.floor(Math.random() * 1000)}
+            ...todos , {text : inputText , completed : false ,id : Math.floor(Math.random() * 1000)}
         ]);
         setInputText("");
     }
+
+    const statusHandler = (e) => {
+        setStatus(e.target.value);
+    }
+    
     return (
         <form>
             <input value = {inputText} onChange = {inputTextHandler} type = "text" className = "todo-input" />
@@ -21,7 +25,7 @@ const Form = ({setInputText , todos ,setTodos, inputText}) => {
                 <i className = "fas fa-plus-square"></i>
             </button>
             <div className = "select">
-                <select name = "todos" className = "filter-todo">
+                <select onChange = {statusHandler} name = "todos" className = "filter-todo">
                     <option value = "all">All</option>
                     <option value = "completed">Completed</option>
                     <option value = "uncompleted">Uncompleted</option>
