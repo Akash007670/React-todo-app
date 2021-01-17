c42d55d2ec52f2bcb42ff799cb074f9e69739d5b
Akash007670 <akaysharma894@gmail.com>
2021-01-17 15:43:58 +0530

    Added filter method

--- todo-app/src/components/TodoList.js
+++ todo-app/src/components/TodoList.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import Todo  from './Todo';
 
-const TodoList = ({todos , setTodos}) => {
+const TodoList = ({todos , setTodos ,filterTodos}) => {
     // console.log(todos);
     return (
         <div className = "todo-container">
             <ul className = "todo-list">
-                {todos.map(todo => (
+                {filterTodos.map(todo => (
                     <Todo 
                         key = {todo.id}
                         text = {todo.text}
                         todos = {todos}
                         setTodos = {setTodos}
                         todo = {todo}
+                        filterTodos = {filterTodos}
                     />
                 ))}
             </ul>            
