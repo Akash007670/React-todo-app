b837e08eb7bf5b5d51ab8adddd1311b755ee447e
Akash007670 <akaysharma894@gmail.com>
2021-03-18 20:29:09 +0530

    configured linter

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React , {useState , useEffect} from 'react';
-import './App.css';
-import Form from './components/Form';
-import TodoList from './components/TodoList';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
-
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
-  const [status, setStatus] = useState("all")
+  const [status, setStatus] = useState("all");
   const [filterTodos, setFilterTodos] = useState([]);
 
   useEffect(() => {
     getLocalTodos();
-  },[])
+  }, []);
 
   useEffect(() => {
     filterHandler();
     saveLocalTodos();
-  },[todos , status]);
-  
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [todos, status]);
+
   const filterHandler = () => {
-    switch(status){
+    switch (status) {
       case "completed":
         setFilterTodos(todos.filter((todo) => todo.completed === true));
         break;
@@ -31,34 +31,34 @@ function App() {
         setFilterTodos(todos);
         break;
     }
-  }
+  };
 
   const saveLocalTodos = () => {
-    localStorage.setItem('todos',JSON.stringify(todos));
-  }
+    localStorage.setItem("todos", JSON.stringify(todos));
+  };
 
   const getLocalTodos = () => {
-    if(localStorage.getItem('todos') === null){
-      localStorage.setItem('todos',JSON.stringify([]));
-    }else{
+    if (localStorage.getItem("todos") === null) {
+      localStorage.setItem("todos", JSON.stringify([]));
+    } else {
       let todoLocal = JSON.parse(localStorage.getItem("todos"));
       setTodos(todoLocal);
     }
-  }
+  };
 
   return (
     <div className="App">
       <header>
         <h1>Akash's Todo list</h1>
       </header>
-      <Form 
-         todos = {todos}
-         setTodos = {setTodos} 
-         setInputText = {setInputText}
-         inputText = {inputText}
-         setStatus = {setStatus}
+      <Form
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+        inputText={inputText}
+        setStatus={setStatus}
       />
-      <TodoList filterTodos = {filterTodos} setTodos = {setTodos} todos = {todos}/>
+      <TodoList filterTodos={filterTodos} setTodos={setTodos} todos={todos} />
     </div>
   );
 }
